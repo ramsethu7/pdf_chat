f9de0ccd7054008dbeb2a564c1018e0189aa7273
ramsethu7 <radhakrish5k126@gmail.com>
2024-05-13 11:12:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-__import__('pysqlite3')
+import pysqlite3
 import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+#then import 
+import chromadb
 
 import streamlit as st
 import langchain
